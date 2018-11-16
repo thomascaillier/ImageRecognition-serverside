ad02ba446220e0defa12d65e6ec3a7411187191e
thomascaillier <thomascaillier@gmail.com>
2018-11-16 17:34:20 +0100

    Add requirements.txt

--- ImageRecognition_serverside/settings.py
+++ ImageRecognition_serverside/settings.py
@@ -53,23 +53,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ImageRecognition_serverside.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = 'ImageRecognition_serverside.wsgi.application'
 
 
