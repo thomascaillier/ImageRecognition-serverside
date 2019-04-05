99eb24e358cb9e7a454739ae55da0ff60f4f4aa4
thomas <thomascaillier@gmail.com>
2019-04-05 17:48:57 +0200

    Returned result are now lists of corresponding images

--- processor/Processor.py
+++ processor/Processor.py
@@ -1,3 +1,13 @@
+import base64
+import io
+import json
+
+from numpy import random
+
+ID_KEY = 'Id_'
+IMAGE_KEY = 'image_'
+PROBABILITY_KEY = 'probability_'
+
 
 class Processor:
 
@@ -5,9 +15,14 @@ class Processor:
     def process(image):
         image.zone_detected_image.save('IMG_' + str(image.id) + '.jpg', image.base_image)
         data = {
-            'wikipedia_url': 'https://wikipedia.com/sample',
-            'logo': 'https://www.usine-digitale.fr/mediatheque/5/0/0/000305005_homePageUne/logo-google-g.jpg'
+            'ResultImages': []
         }
+        for i in range(4):
+            '''with open(image.zone_detected_image.path, 'rb') as f:'''
+            data.get('ResultImages').append({
+                ID_KEY: 666 + i,
+                IMAGE_KEY: base64.encodebytes(bytearray(io.open(image.zone_detected_image.path, 'rb').read())).decode('utf-8'),
+                PROBABILITY_KEY: random.random()
+            })
         image.corresponding_data = data
         return True
-
